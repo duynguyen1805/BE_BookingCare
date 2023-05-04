4e2be58d144b7e582f2eac0846a93538b43f54c0
TDN <duynguyenqwert@gmail.com>
2023-05-04 12:50:53 +0700

    4/5 final

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -2,7 +2,7 @@ import doctorService from "../services/doctorService";
 
 let getTopDoctor = async (req, res) => {
   let limit = req.query.limit;
-  if (!limit) limit = 10;
+  if (!limit) limit = 12;
   try {
     let doctors = await doctorService.getTopDoctorHome(+limit);
     return res.status(200).json(doctors);
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,40 +4,13 @@ import userController from "../controller/userController";
 import doctorController from "../controller/doctorController";
 import specialtyController from "../controller/specialtyController";
 import patientController from "../controller/patientController";
-import multer from "multer";
-import path from "path";
 
 var appRoot = require("app-root-path");
 let router = express.Router();
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, appRoot + "/src/public/image/"); //appRoot: /thumucroot
-//   },
-
-//   filename: function (req, file, cb) {
-//     // dat filename khong trung
-//     cb(
-//       null,
-//       file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-//     );
-//   },
-// });
-// const imageFilter = (req, file, cb) => {
-//   //accept images only
-//   if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-//     req.fileValidationError = "only image files are allowed!";
-//     return cb(new Error("only image files are allowed!"), false);
-//   }
-//   cb(null, true);
-// };
-// let upload = multer({ storage: storage, fileFilter: imageFilter });
-
 const initWebRoutes = (app) => {
   //method: post, get, put, delete
-  //method get => client lay thong tin tu servers tra ve.
   router.get("/", homeController.getHomepage);
-
   router.get("/crud", homeController.getCrudpage);
   router.post("/post-crud", homeController.postCRUD);
   router.get("/get-crud", homeController.displayGetCRUD);
